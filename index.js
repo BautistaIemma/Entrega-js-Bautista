eef8f6ec07fc4a28fae21c891b9cf595780dbb81
BautistaIemma <106565730+BautistaIemma@users.noreply.github.com>
2022-10-24 15:16:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Ciclo con For y condicionales 
+
+const num1 = 5
+
+for (let i = 0; i <= num1; i++){
+
+    //Condicionales
+
+    if (i === num1){
+        alert ("llegamos al numero 10")
+    }else{
+        alert (`estamos en la iteracion ${i}`)
+    }
+}
